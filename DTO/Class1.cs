cdb39eb654da0cbabd83930537e75f491fc8b83d
Nguyen Thien <cristhuuthien@gmail.com>
2023-12-16 00:25:21 +0700

    test commit in vs2019

---
+++ DTO/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Class1
+    {
+        public void Check()
+        {
+            Console.WriteLine("Thine");
+            Console.WriteLine("Thine");
+
+        }
+    }
+}
