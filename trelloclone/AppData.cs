9b32a8861cc2eb596c369541978710d680ad400f
DESKTOP-DLQ5TTI\Dell <anhtuannguyendai@gmail.com>
2023-12-17 12:58:22 +0700

    Change card handler

--- trelloclone/AppData.cs
+++ trelloclone/AppData.cs
@@ -6,21 +6,43 @@ using System.Threading.Tasks;
 
 namespace trelloclone
 {
-    internal class AppData
+    public class AppData
     {
+        private string title;
         private string describeContent;
         private string activityContent;
-        private string memberAccessID;
+        private string label;
+        private List<string> listOfMember = new List<string>();
+        private List<string> listOfToDoList = new List<string>();
+        private bool existed;
 
+        public string Title { get => title; set => title = value; }
         public string DescribeContent { get => describeContent; set => describeContent = value; }
         public string ActivityContent { get => activityContent; set => activityContent = value; }
-        public string MemberAccessID { get => memberAccessID; set => memberAccessID = value; }
+        public string Label { get => label; set => label = value; }
+        public List<string> ListOfMember { get => listOfMember; set => listOfMember = value; }
+        public List<string> ListOfToDoList { get => listOfToDoList; set => listOfToDoList = value; }
+        public bool Existed { get => existed; set => existed = value; }
+
+        public AppData()
+        {
+            title = "Tiêu đề";
+            describeContent = "";
+            activityContent = "";
+            label = "none";
+            Existed = true;
+        }
 
         public AppData(string describeContent, string activityContent, string memberAccessID)
         {
             DescribeContent = describeContent;
             ActivityContent = activityContent;
-            MemberAccessID = memberAccessID;
+            listOfMember.Add(memberAccessID);
+        }
+        ~AppData()
+        {
+
+
         }
 
 
--- trelloclone/InsideTable.cs
+++ trelloclone/InsideTable.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,139 +13,188 @@ namespace trelloclone
 {
     internal class InsideTable
     {
-        List<Guna2Button> listOfBtn = new List<Guna2Button>();
-        List<Panel> listOfList = new List<Panel>();
-
-        
         Panel pnlAllList = new Panel();
         Panel pnlFirstList = new Panel();
-        
         Guna2Button btnAddNewCard = new Guna2Button();
         Guna2Button btnCard = new Guna2Button();
-        //listOfBtn.add(btnCard);
         Guna2Button btnAddNewList = new Guna2Button();
 
-        
+        public Panel PnlAllList { get => pnlAllList; set => pnlAllList = value; }
+        public Panel PnlFirstList { get => pnlFirstList; set => pnlFirstList = value; }
+        public Guna2Button BtnAddNewCard { get => btnAddNewCard; set => btnAddNewCard = value; }
+        public Guna2Button BtnCard { get => btnCard; set => btnCard = value; }
+        public Guna2Button BtnAddNewList { get => btnAddNewList; set => btnAddNewList = value; }
 
-        private void btnStoryCard1_List1_Click(object sender, EventArgs e)
+        public InsideTable(Form1 form, Panel WorkSpace, Panel TableSpace)
         {
-            Card a = new Card();
+            WorkSpace.Controls.Add(PnlAllList);
+            PnlAllList.Size = WorkSpace.Size;
+            PnlAllList.BackColor = WorkSpace.BackColor;
+            PnlAllList.Controls.Add(BtnAddNewList);
 
-            a.Show();
+            BtnAddNewList.BorderRadius = 5;
+            BtnAddNewList.BackColor = Color.Transparent;
+            BtnAddNewList.FillColor = Color.FromArgb(224, 224, 224);
+            BtnAddNewList.Size = new Size(130, 30);
+            BtnAddNewList.Location = new Point(200, 10);
+            BtnAddNewList.Image = Image.FromFile("C:\\Users\\Dell\\source\\repos\\trelloclone\\trelloclone\\Resources\\Add.png");
+            BtnAddNewList.ImageAlign = HorizontalAlignment.Left;
+            BtnAddNewList.ImageOffset = new Point(-10, 0);
+            BtnAddNewList.Text = "Thêm danh sách";
+            BtnAddNewList.ForeColor = Color.Black;
+            BtnAddNewList.TextOffset = new Point(-1, 0);
+            BtnAddNewList.Click += btnAddNewList_Click;
+
+            createTheFirstList(pnlAllList);
 
         }
-        public InsideTable(Form1 form, Panel WorkSpace, Panel TableSpace)
-        { 
+        private void createTheFirstList(Panel PnlAllList)
+        {
+            list firstList = new list(PnlAllList);
+            firstList.BtnAddCard.Click += btnAddCard_Click;
+            firstList.BtnFirstCard.Click += DynamicButton_Click;
+            firstList.BtnDeleteList.Click += btnDeleteList_Click;
+            firstList.BtnMoveToLeft.Click += BtnMoveToLeft_Click;
+            firstList.BtnMoveToRight.Click += BtnMoveToRight_Click;
+        }
 
-            WorkSpace.Controls.Add(pnlAllList);
-            pnlAllList.Size = WorkSpace.Size;
-            // pnlAllList.BackColor = WorkSpace.BackColor;
-            pnlAllList.BackColor = WorkSpace.BackColor;
+        private void BtnMoveToRight_Click(object sender, EventArgs e)
+        {
+            Guna2Button clickedButton = sender as Guna2Button;
+            list form = list.FindForm(clickedButton);
 
-            pnlAllList.Controls.Add(pnlFirstList); //Add panel for list
-            pnlFirstList.Size = new Size(260, 110);
-            pnlFirstList.Location = new Point(200, 50);
-            pnlFirstList.BackColor = Color.White;
-            listOfList.Add(pnlFirstList); //Add first list to the LIST of panel
+            if (form != null)
+            {
+                int index = list.ListOfList.IndexOf(form);
 
-            pnlAllList.Controls.Add(btnAddNewList);
-            btnAddNewList.BorderRadius = 5;
-            btnAddNewList.BackColor = Color.Transparent;
-            btnAddNewList.FillColor = Color.FromArgb(224, 224, 224);
-            btnAddNewList.Size = new Size(130, 30);
-            btnAddNewList.Location = new Point(200, 10);
-            btnAddNewList.Image = Image.FromFile(Application.StartupPath + "/Resources/Add.png");
-            btnAddNewList.ImageAlign = HorizontalAlignment.Left;
-            btnAddNewList.ImageOffset = new Point(-10, 0);
-            btnAddNewList.Text = "Thêm danh sách";
-            btnAddNewList.ForeColor = Color.Black;
-            btnAddNewList.TextOffset = new Point(-1, 0);
+                if (index + 1 == list.ListOfList.Count) // check for first and last table
+                {
+                    return;
+                }
+                else
+                {
+                    swap(list.ListOfList[index], list.ListOfList[index + 1], index, index + 1);
+                    list temp = list.ListOfList[index];
+                    list.ListOfList[index] = list.ListOfList[index + 1];
+                    list.ListOfList[index + 1] = temp;
+                }
+            }
+        }
 
-            pnlFirstList.Controls.Add(btnCard); //Add the card button for list
-            btnCard.BackColor = Color.Transparent;
-            btnCard.BorderRadius = 5;
-            btnCard.FillColor = Color.FromArgb(224, 224, 224);
-            btnCard.Location = new Point(5, 40);
-            btnCard.Size = new Size(250, 35);
-            btnCard.ForeColor = Color.Black;
-            btnCard.Text = "Content...";
-            btnCard.TextAlign = HorizontalAlignment.Left;
-            listOfBtn.Add(btnCard); //add the btn Card to the LIST of button
+        private void BtnMoveToLeft_Click(object sender, EventArgs e)
+        {
+            Guna2Button clickedButton = sender as Guna2Button;
+            list form = list.FindForm(clickedButton);
 
-            pnlFirstList.Controls.Add(btnAddNewCard);
-            btnAddNewCard.BorderRadius = 5; 
-            btnAddNewCard.BackColor = Color.White;
-            btnAddNewCard.FillColor = Color.FromArgb(224, 224, 224);
-            btnAddNewCard.Size = new Size(100, 20);
-            btnAddNewCard.Location = new Point(listOfBtn[listOfBtn.Count - 1].Location.X, listOfBtn[listOfBtn.Count - 1].Location.Y+40);
-            btnAddNewCard.Image = Image.FromFile(Application.StartupPath + "/Resources/Add.png");
-            btnAddNewCard.ImageAlign = HorizontalAlignment.Left;
-            btnAddNewCard.ImageOffset = new Point(-10, 0);
-            btnAddNewCard.Text = "Thêm thẻ";
-            btnAddNewCard.ForeColor = Color.Black;
-            btnAddNewCard.TextOffset = new Point(-1, 0);
+            if (form != null)
+            {
+                int index = list.ListOfList.IndexOf(form);
+                if (index == 0) // check for first and last table
+                    return;
+                else
+                {
+                    swap(list.ListOfList[index - 1], list.ListOfList[index], index - 1, index);
+                    list temp = list.ListOfList[index - 1];
+                    list.ListOfList[index - 1] = list.ListOfList[index];
+                    list.ListOfList[index] = temp;
+                }
+            }
+        }
+        private void swap(list a, list b, int index_a, int index_b)
+        {
+            Point c = a.Location;
+            a.Location = b.Location;
+            b.Location = c;
+        }
 
-            btnAddNewCard.Click += btnAddNewCard_Click;
-            btnAddNewList.Click += btnAddNewList_Click;
+        private void btnAddCard_Click(object sender, EventArgs e)
+        {
+            Guna2Button clickedButton = sender as Guna2Button;
+            list form = list.FindForm(clickedButton);
+
+            if (form != null)
+            {
+                form.createOtherCard(form.PnlCurrentList);
+                form.ListOfBtn[form.ListOfBtn.Count - 1].Click += DynamicButton_Click;
+                form.modifyAddCard(form.PnlCurrentList);
+                form.modifyDeleteList(form.PnlCurrentList);
+                form.BtnDeleteList.Click += btnDeleteList_Click;
+            }
         }
         private void btnAddNewList_Click(object sender, EventArgs e)
         {
-
-            MessageBox.Show("...");
-
-            //pnlFirstList.Controls.Add(btnCard); //Add the card button for list
-            //btnCard.BackColor = Color.Transparent;
-            //btnCard.BorderRadius = 5;
-            //btnCard.FillColor = Color.FromArgb(224, 224, 224);
-            //btnCard.Location = new Point(5, 40);
-            //btnCard.Size = new Size(450, 35);
-            //btnCard.ForeColor = Color.Black;
-            //btnCard.Text = "Content...";
-            //btnCard.TextAlign = HorizontalAlignment.Left;
-            //listOfBtn.Add(btnCard); //add the btn Card to the LIST of button
-
-            //pnlFirstList.Controls.Add(btnAddNewCard);
-            //btnAddNewCard.BorderRadius = 5;
-            //btnAddNewCard.BackColor = Color.White;
-            //btnAddNewCard.FillColor = Color.FromArgb(224, 224, 224);
-            //btnAddNewCard.Size = new Size(100, 20);
-            //btnAddNewCard.Location = new Point(listOfBtn[listOfBtn.Count - 1].Location.X, listOfBtn[listOfBtn.Count - 1].Location.Y + 40);
-            //btnAddNewCard.Image = Image.FromFile("C:\\Users\\Dell\\source\\repos\\trelloclone\\trelloclone\\Resources\\Add.png");
-            //btnAddNewCard.ImageAlign = HorizontalAlignment.Left;
-            //btnAddNewCard.ImageOffset = new Point(-10, 0);
-            //btnAddNewCard.Text = "Thêm thẻ";
-            //btnAddNewCard.ForeColor = Color.Black;
-            //btnAddNewCard.TextOffset = new Point(-1, 0);
+            if (list.ListOfList.Count != 0)
+            {
+                list anotherList = new list(PnlAllList);
+                //pnlList.Add(anotherList);
+                anotherList.BtnAddCard.Click += btnAddCard_Click;
+                anotherList.BtnFirstCard.Click += DynamicButton_Click;
+                anotherList.BtnDeleteList.Click += btnDeleteList_Click;
+                anotherList.BtnMoveToLeft.Click += BtnMoveToLeft_Click;
+                anotherList.BtnMoveToRight.Click += BtnMoveToRight_Click;
+            }
+            else
+                createTheFirstList(PnlAllList);
         }
-
-        private void btnAddNewCard_Click(object sender, EventArgs e)
+        private void btnDeleteList_Click(object sender, EventArgs e)
         {
-            listOfList[listOfList.Count-1].Size = new Size(listOfList[listOfList.Count - 1].Size.Width, listOfList[listOfList.Count - 1].Size.Height+40);
-            btnAddNewCard.Location = new Point(btnAddNewCard.Location.X, btnAddNewCard.Location.Y + 40);
+            Guna2Button clickedButton = sender as Guna2Button;
+            list currentList = list.FindForm(clickedButton);
 
-            Guna2Button btnCard_in = new Guna2Button();
-            btnCard.Click += DynamicButton_Click;
-            btnCard_in.Click += DynamicButton_Click;
-            btnCard_in.Location = new Point(listOfBtn[listOfBtn.Count - 1].Location.X,
-                listOfBtn[listOfBtn.Count - 1].Location.Y + 40);
-            btnCard_in.BackColor = listOfBtn[listOfBtn.Count - 1].BackColor;
-            btnCard_in.Size = listOfBtn[listOfBtn.Count - 1].Size;
-            btnCard_in.BackColor = Color.Transparent;
-            btnCard_in.BorderRadius = listOfBtn[listOfBtn.Count - 1].BorderRadius;
-            btnCard_in.FillColor = listOfBtn[listOfBtn.Count - 1].FillColor;
-            btnCard_in.TextAlign = HorizontalAlignment.Left;
-            btnCard_in.ForeColor = Color.Black;
-            btnCard_in.Text = "Content...";
+            if (currentList != null)
+            {
+                DialogResult result = MessageBox.Show("Thực hiện xóa danh sánh?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-            listOfList[listOfList.Count - 1].Controls.Add(btnCard_in);
-            listOfBtn.Add(btnCard_in);
+                if (result == DialogResult.Yes)
+                {
+                    Point oldLocation = currentList.Location;
+                    int index = list.ListOfList.IndexOf(currentList);
 
+                    if (list.ListOfList.Count >= 1)
+                    {
+                        list.ListOfList.RemoveAt(index);
+                        Size oldSize = currentList.Size;
+                        Point OldLocation = currentList.Location;
+                        pnlAllList.Controls.Remove(currentList);
+                        list.moveListAfterDelete(OldLocation, oldSize);
+                    }
+                }
+            }
         }
         private static void DynamicButton_Click(object sender, EventArgs e)
         {
-            Card a = new Card();
-            MessageBox.Show("hel");
-            a.Show();
+            Guna2Button clickedButton = sender as Guna2Button;
+            list form = list.FindForm(clickedButton);
+
+            if (form != null)
+            {
+                int index = form.ListOfBtn.IndexOf(clickedButton);
+                Card a = new Card(form.ListOfData[index]);
+
+                a.ShowDialog();
+
+                if (a.Existed)
+                {
+                    form.ListOfData[index] = a.AppData;
+                    form.ListOfBtn[index].Text = a.AppData.Title;
+                }
+                else
+                {
+                    if (index == 0)
+                    {
+                        form.ListOfData.RemoveAt(index);
+                    }
+                    else
+                    {
+                        Point OldLocation = clickedButton.Location;
+                        form.Controls.Remove(clickedButton);
+                        list.moveCardAfterDelete(form, OldLocation);
+                        form.ListOfData.RemoveAt(index);
+                        form.ListOfBtn.RemoveAt(index);
+                    }
+                }
+            }
         }
+
     }
 }
